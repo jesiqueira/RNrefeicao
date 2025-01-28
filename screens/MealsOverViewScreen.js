0102965249be80a9dd400a4308576e82edd42d60
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-01-28 17:35:06 -0300

    adicionar style e criado componente MealItem

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,16 +1,32 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 
 import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const MealsOverViewScreen = ({ route }) => {
   const catId = route.params.categoryId
 
-  const displayedMeals = MEALS.filter(()=>{})
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0
+  })
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    }
+    return <MealItem {...mealItemProps} />
+  }
 
   return (
     <View style={styles.container}>
       {/*Tela de visão geral das refeições */}
-      <Text>Meals Overview Screen {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
     </View>
   )
 }
